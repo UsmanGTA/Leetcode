70ac2125d228eaa8ba2181d992fd321c9436b395
UsmanGTA <hi@usmanjabbar.com>
2021-02-11 08:19:41 +0000

    Added initial commits

---
+++ linked_list_algorithms/0-check_cycle.c
@@ -0,0 +1,167 @@
+/* HEADER FILE
+	===================================== */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+/* HEADER FILE END ========================= */
+
+
+
+/* MAIN LOGIC FILE ( CYCLE CHECK )
+	========================================= */
+
+/**
+ * check_cycle - check cycle is a function that checks
+ * whether a linked list is in a loop or not.
+ * @list: beginning of the linked list
+ * Return: 0 if no loop | 1 if loop detected
+ */
+int check_cycle(listint_t *list) {
+	listint_t *s_leap, *d_leap;
+
+	if (!list)
+		return(0);
+
+	for (s_leap = list, d_leap = list->next; d_leap && d_leap->next; s_leap = s_leap->next, d_leap = d_leap->next->next)
+		if (s_leap == d_leap)
+			return(1);
+
+	return(0);
+}
+
+
+
+/* MISC HELPER FUNCTIONS
+	========================================= */
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
+
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
+
+    return (n);
+}
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: pointer to a pointer of the start of the list
+ * @n: integer to be included in node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *new;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = *head;
+    *head = new;
+
+    return (new);
+}
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
+
+/* MISC HELPER FUNCTION ENDS
+	========================================= */
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    listint_t *current;
+    listint_t *temp;
+    int i;
+
+    head = NULL;
+    add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint(head);
+
+    if (check_cycle(head) == 0)
+        printf("Linked list has no cycle\n");
+    else if (check_cycle(head) == 1)
+        printf("Linked list has a cycle\n");
+
+    current = head;
+    for (i = 0; i < 4; i++)
+        current = current->next;
+    temp = current->next;
+    current->next = head;
+
+    if (check_cycle(head) == 0)
+        printf("Linked list has no cycle\n");
+    else if (check_cycle(head) == 1)
+        printf("Linked list has a cycle\n");
+
+    current = head;
+    for (i = 0; i < 4; i++)
+        current = current->next;
+    current->next = temp;
+
+    free_listint(head);
+
+    return (0);
+}
